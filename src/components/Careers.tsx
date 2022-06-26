b39a90a38f7ea69a63f5958a67e080018c6d1ed0
Kien Do <kienmarkdo@gmail.com>
2022-06-26 19:57:00 -0400

    Conversion to BlueprintJS completed

--- src/components/Careers.tsx
+++ src/components/Careers.tsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function Careers() {
-  return <div>Careers</div>;
+  return <>Careers</>;
 }
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,85 +1,18 @@
+import { Button } from "@blueprintjs/core";
 import React from "react";
-import { Button, Container, Row, Col, Image } from "react-bootstrap";
 
 export default function Home() {
   return (
-    <>
-      <Container>
-        <div style={{ margin: "50px" }}>
-          <h1 style={{ width: "60%" }}>
-            90% of Canadian businesses experienced a cyber attack in 2022
-          </h1>
-          <br />
-          <h3>Don't be another statistic</h3>
-          <br />
-          <Button variant="success">Learn more</Button>
-        </div>
-      </Container>
-      <hr />
-      <br />
-      <div style={{}}>
-        {" "}
-        <Container style={{ display: "flex", justifyContent: "center" }}>
-          <h1>Our Partners</h1>
-        </Container>
+    <div>
+      <div style={{ margin: "50px 100px" }}>
+        <h1 className="bp4-heading" style={{ width: "60%" }}>
+          90% of Canadian businesses experienced a cyber attack in 2022
+        </h1>
         <br />
-        <Container
-          style={{
-            display: "flex",
-            justifyContent: "space-evenly",
-          }}
-        >
-          <Row>
-            <Col sm={3}>
-              <Image
-                src="https://letsteachtheworld.org/wp-content/uploads/2016/07/myce-microsoft-Logo-2-300x85.png"
-                alt=""
-                style={{ width: "100%" }}
-              />
-            </Col>
-            <Col sm={3}>
-              <Image
-                src="https://149360579.v2.pressablecdn.com/wp-content/uploads/2020/01/government-of-canada-logo-Copy.png"
-                alt=""
-                style={{ width: "100%" }}
-              />
-            </Col>
-            <Col sm={3}>
-              <Image
-                src="https://1000logos.net/wp-content/uploads/2018/10/RBC-Logo.png"
-                alt=""
-                style={{ width: "100%" }}
-              />
-            </Col>
-            <Col sm={3}>
-              <Image
-                src="https://www.vmcdn.ca/f/files/shared/corporate-logos/_logo_theottawahospital_1500x600.jpg"
-                alt=""
-                style={{ width: "100%" }}
-              />
-            </Col>
-          </Row>
-        </Container>
+        <h3 className="bp4-heading">Don't be another statistic</h3>
+        <br />
+        <Button intent="success">Learn more</Button>
       </div>
-
-      <br />
-      <hr />
-      <br />
-      <Container style={{ display: "flex", justifyContent: "center" }}>
-        <h1>Our Solutions</h1>
-      </Container>
-      <br />
-      <Container style={{ display: "flex", justifyContent: "space-evenly" }}>
-        <Row>
-          <Col sm={2}>Prevention and Protection</Col>
-          <Col sm={1}></Col>
-          <Col sm={2}>Emergency Response</Col>
-          <Col sm={1}></Col>
-          <Col sm={2}>Simulated Environments</Col>
-          <Col sm={1}></Col>
-          <Col sm={2}>Consultation</Col>
-        </Row>
-      </Container>
-    </>
+    </div>
   );
 }
