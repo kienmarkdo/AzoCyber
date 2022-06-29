dd90ad32ccf09508fc5a69050f29bcdb6038ea5b
Kien Do <kienmarkdo@gmail.com>
2022-06-29 15:56:37 -0400

    Finished Personal Information Form

--- src/components/FormPersonalInformation.tsx
+++ src/components/FormPersonalInformation.tsx
@@ -1,5 +1,85 @@
-import React from "react";
+import { Button, FormGroup, Icon, InputGroup } from "@blueprintjs/core";
+import { useNavigate } from "react-router";
 
 export default function FormPersonalInformation() {
-  return <div>FormPersonalInformation</div>;
+  const navigate = useNavigate();
+
+  const routeToBusinessInfo = () => {
+    navigate("/business_information");
+  };
+
+  return (
+    <>
+      <section className="formContainer">
+        <div className="progressBar">
+          <div className="iconGroup">
+            <Icon
+              icon="tick-circle"
+              iconSize={100}
+              intent="success"
+              className="iconStyle"
+            />
+            <p>Personal Information</p>
+          </div>
+
+          <Icon icon="arrow-right" iconSize={50} style={{ color: "#8F99A8" }} />
+          <div className="iconGroup">
+            <Icon
+              icon="ban-circle"
+              iconSize={100}
+              className="iconStyle incomplete"
+            />
+            <p>Business Information</p>
+          </div>
+
+          <Icon icon="arrow-right" iconSize={50} style={{ color: "#8F99A8" }} />
+          <div className="iconGroup">
+            <Icon
+              icon="ban-circle"
+              iconSize={100}
+              className="iconStyle incomplete"
+            />
+            <p>Security Information</p>
+          </div>
+        </div>
+      </section>
+      <section className="formInputContainer">
+        <FormGroup
+          className=""
+          label="Full Name"
+          labelFor="name-input"
+          labelInfo="(required)"
+        >
+          <InputGroup id="name-input" placeholder="Enter Your Full Name" />
+        </FormGroup>
+        <FormGroup
+          label="Email Address"
+          labelFor="email-input"
+          labelInfo="(required)"
+        >
+          <InputGroup id="email-input" placeholder="Enter Your Email Address" />
+        </FormGroup>
+        <FormGroup
+          label="Phone Number"
+          labelFor="phone-input"
+          labelInfo="(required)"
+        >
+          <InputGroup id="phone-input" placeholder="Enter Your Phone Number" />
+        </FormGroup>
+        <FormGroup
+          label="Position"
+          labelFor="position-input"
+          labelInfo="(required)"
+        >
+          <InputGroup
+            id="position-input"
+            placeholder="Enter Your Position (e.g.: Security Analyst, Manager, etc.)"
+          />
+        </FormGroup>
+        <Button intent="success" large={true} onClick={routeToBusinessInfo}>
+          Next
+        </Button>
+      </section>
+    </>
+  );
 }
--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,19 +1,24 @@
-import FormBusinessInformation from "../components/FormBusinessInformation";
 import FormPersonalInformation from "../components/FormPersonalInformation";
+import FormBusinessInformation from "../components/FormBusinessInformation";
 import FormSecurityInformation from "../components/FormSecurityInformation";
+import { Route, Routes } from "react-router";
 
 export default function Contact() {
   return (
     <>
-      <h1
-        className="bp4-heading"
-        style={{ textAlign: "center", marginTop: "75px" }}
-      >
-        Enter Contact Information
-      </h1>
-      <FormPersonalInformation />
-      <FormBusinessInformation />
-      <FormSecurityInformation />
+      <h1 className="bp4-heading formHeader">Enter Contact Information</h1>
+      <Routes>
+        <Route path="/" element={<FormPersonalInformation />} />
+        <Route
+          path="/personal_information"
+          element={<FormPersonalInformation />}
+        />
+        <Route
+          path="/business_information"
+          element={<FormBusinessInformation />}
+        />
+        {/* <Route path="*" element={<FormPersonalInformation />} /> */}
+      </Routes>
     </>
   );
 }
